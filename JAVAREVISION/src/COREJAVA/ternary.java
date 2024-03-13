2209f76866a6e3d9a9c333b945312b0880e8aeb6
Mahigavale <maheshgavale07@gmail.com>
2024-03-14 00:22:12 +0530

    the ternary operator

---
+++ JAVAREVISION/src/COREJAVA/ternary.java
@@ -0,0 +1,15 @@
+package COREJAVA;
+
+public class ternary {
+
+	public static void main(String[] args) {
+  
+		
+		int num=90;
+		boolean result;
+		result=(num>7)? true:false ;
+		System.out.println(result);
+
+	}
+
+}
