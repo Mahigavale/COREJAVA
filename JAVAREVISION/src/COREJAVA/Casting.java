3edcfc08619993498a8ba0136b4a812df32498f3
Mahigavale <maheshgavale07@gmail.com>
2024-03-13 23:56:34 +0530

    casting

---
+++ JAVAREVISION/src/COREJAVA/Casting.java
@@ -0,0 +1,28 @@
+package COREJAVA;
+
+public class Casting {
+ public static void main(String[] args) {
+	
+	 /* there are two types of castings in java.
+	  * 1)widening-> store small data into big one. default.
+	  * 2)narrowing->store big into small. manual.
+	  * 
+	  * 
+	  * 
+	  */
+	 
+	 double d=8.89d;
+	 
+	 System.out.println(d);
+	 
+	int c=(int)d;
+	 System.out.println(c);
+	 
+	 
+	 int num=123;
+	 double d1=num;
+	 System.out.println(num);
+	 System.out.println(d1);
+	 
+}
+}
